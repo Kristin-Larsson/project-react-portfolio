b74be21bdb451f5cb78aca9cae3b036927f1366c
Kristin <kristin@iMac.local>
2023-06-13 12:00:24 +0200

    spelling corrections

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,13 +1,12 @@
 import React from 'react-dom'
 import { Line, Dot } from '../styles/Intro'
 import { Section, Wrapper, MainText } from '../styles/Main'
-
 const Intro = () => {
   return (
     <Section>
       <Wrapper>
         <MainText> <Line /><Dot />I am a creative team player with a passion for crafting user-friendly solutions and visually captivating designs. 
-        Within my portfolio, I exhibit my past projects, demonstrating my coding prowess and the results i have achieved.
+        Within my portfolio, I exhibit my past projects, demonstrating my coding prowess and the results I have achieved.
         </MainText>
       </Wrapper>
     </Section>
